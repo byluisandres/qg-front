61c32ed4ec6bc2851b0a9f1993c5bb935c4d4ee6
Luis Andrés Bolaños Yapo <luisandres33bolanos@gmail.com>
2022-06-12 18:03:33 +0200

    icons

---
+++ src/Partials/Icon.vue
@@ -0,0 +1,42 @@
+<template>
+    <svg v-if="name === 'home'" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-neutral-500" fill="none"
+        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+    </svg>
+    <svg v-if="name === 'library'" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6  text-neutral-500" fill="none"
+        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+    </svg>
+    <svg v-if="name === 'credit-card'" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-neutral-500" fill="none"
+        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+    </svg>
+    <svg v-if="name === 'archive'" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-neutral-500" fill="none"
+        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+    </svg>
+    <svg v-if="name === 'clipboard-list'" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-neutral-500"
+        fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+    </svg>
+    <svg v-if="name === 'currency-euro'" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-neutral-500" fill="none"
+        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M14.121 15.536c-1.171 1.952-3.07 1.952-4.242 0-1.172-1.953-1.172-5.119 0-7.072 1.171-1.952 3.07-1.952 4.242 0M8 10.5h4m-4 3h4m9-1.5a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'icon',
+    props: ['name']
+}
+</script>
+
+<style>
+</style>
