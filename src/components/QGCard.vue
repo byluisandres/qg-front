2bc8ddef841af653cfefa77128dc3ea14cfb64e4
Luis Andrés Bolaños Yapo <luisandres33bolanos@gmail.com>
2022-06-12 18:03:24 +0200

    componente QGCard

---
+++ src/components/QGCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow rounded-lg  border-0">
+        <div class="rounded-t bg-neutral-900 mb-0 px-4 py-4">
+            <div class="text-center flex justify-between">
+                <h6 class="text-gray-50 text-xl font-bold">
+                    {{ title }}
+                </h6>
+            </div>
+        </div>
+        <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QGCard',
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+</style>
