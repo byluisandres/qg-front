d0849acef9596520c78a2cf4c1b974d97c906add
Luis Andrés Bolaños Yapo <luisandres33bolanos@gmail.com>
2022-06-12 18:03:06 +0200

    rutas

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,21 @@ const routes = [
         name: 'dashboard',
         component: () => import('../views/pages/dashboard.vue')
     },
+    {
+        path: '/cuentas-bancarias',
+        name: 'cuentas-bancarias',
+        component: () => import('../views/pages/bankAccounts/index.vue')
+    },
+    {
+        path: '/cuentas-bancarias/new',
+        name: 'cuentas-bancarias-new',
+        component: () => import('../views/pages/bankAccounts/edit.vue')
+    },
+    {
+        path: '/cuentas-bancarias/:id',
+        name: 'cuentas-bancarias-edit',
+        component: () => import('../views/pages/bankAccounts/edit.vue')
+    }
 ]
 
 export default routes;
