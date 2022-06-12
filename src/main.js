f9b8e313476cc7eae7c28851149eae6f60a8b56c
Luis Andrés Bolaños Yapo <luisandres33bolanos@gmail.com>
2022-06-12 18:02:38 +0200

    instalado mdi,sweetalert2,sass

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css';
+import '@mdi/font/css/materialdesignicons.min.css'
 import Oruga from '@oruga-ui/oruga-next';
 import '@oruga-ui/oruga-next/dist/oruga.css';
 import '@oruga-ui/oruga-next/dist/oruga-full.css'
 import router from './router';
 import { createPinia } from 'pinia'
+import Swal from 'sweetalert2'
+import 'sweetalert2/src/sweetalert2.scss'
+import '@sweetalert2/theme-dark';
 
-
-createApp(App).use(router).use(createPinia()).use(Oruga).mount('#app')
+const app = createApp(App);
+app.use(Oruga)
+app.use(router)
+app.use(createPinia())
+app.config.globalProperties.$swal = Swal
+app.mount('#app')
